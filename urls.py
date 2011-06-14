1cc7c1f21751e12d9e0c62ad2ceb66580f29e8de
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-06-14 12:59:53 -0400

    add export/import

--- urls.py
+++ urls.py
@@ -7,6 +7,9 @@ admin.autodiscover()
 site_media_root = os.path.join(os.path.dirname(__file__),"media")
 
 urlpatterns = patterns('',
+                       (r'^export/','main.views.exporter'),
+                       (r'^import/','main.views.importer'),
+
                        ('^accounts/',include('djangowind.urls')),
                        (r'^admin/(.*)', admin.site.root),
                        (r'^munin/total_stands/','main.views.total_stands'),

e2816ab799d390c1e03ff21913f82609c9cfeb26
Anders Pearson <anders@columbia.edu>
2011-10-20 15:48:43 -0400

    update to pageblocks 0.5.3 (with south migrations)

--- main/models.py
+++ main/models.py
@@ -4,6 +4,15 @@ import hashlib
 from django.db.models import get_model
 from django.conf import settings
 
+from south.modelsinspector import add_introspection_rules
+
+add_introspection_rules([], 
+                        ["^django_extensions\.db\.fields\.CreationDateTimeField",
+                         "django_extensions.db.fields.ModificationDateTimeField",
+                         "sorl.thumbnail.fields.ImageWithThumbnailsField",
+                         "django_extensions.db.fields.UUIDField"])
+
+
 class Stand(models.Model):
     title = models.CharField(max_length=256,default=u"",blank=True,null=True)
     hostname = models.CharField(max_length=256,db_index=True)

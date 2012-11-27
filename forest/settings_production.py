442c14e4c6c275078db5734a0a913a9c5a393908
Anders Pearson <anders@columbia.edu>
2012-11-27 13:30:54 -0500

    convert to new style database config

--- forest/settings_production.py
+++ forest/settings_production.py
@@ -8,13 +8,24 @@ TEMPLATE_DIRS = (
 MEDIA_ROOT = '/var/www/forest/uploads/'
 # put any static media here to override app served static media
 STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/forest/forest/sitemedia'),	
+    ('/sitemedia', '/var/www/forest/forest/sitemedia'),
 )
 
 COMPRESS_ROOT = "/var/www/forest/forest/media/"
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'forest',
+        'HOST': '',
+        'PORT': 6432,
+        'USER': '',
+        'PASSWORD': '',
+        }
+}
 
 try:
     from local_settings import *

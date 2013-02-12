5e6aac939b173cba808f8b78792f3e7e22841af2
Anders Pearson <anders@columbia.edu>
2013-02-12 15:51:34 -0500

    django admin needs messages middleware in 1.4

--- forest/settings_shared.py
+++ forest/settings_shared.py
@@ -68,6 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',

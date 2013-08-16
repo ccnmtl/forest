b3276315f95f11e5cc9cf7110b7f92c4838857c7
Anders Pearson <anders@columbia.edu>
2013-08-16 15:32:28 -0400

    wheelify

--- bootstrap.py
+++ bootstrap.py
@@ -12,34 +12,28 @@ if os.path.exists(vedir):
 
 virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "requirements", "virtualenv_support"))
 
-ret = subprocess.call(["python", "virtualenv.py",
+ret = subprocess.call(["python", "virtualenv.py", 
                        "--extra-search-dir=%s" % virtualenv_support_dir,
                        "--never-download",
                        vedir])
 if ret: exit(ret)
 
-if sys.version_info < (2, 7, 0):
-    ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                           "-E", vedir,
-                           os.path.join(pwd,"requirements/src/Imaging-1.1.7.tar.gz")])
-else:
-    ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                           "-E", vedir,
-                           os.path.join(pwd,"requirements/src/Pillow-1.7.8.zip")])
+ret = subprocess.call(
+    [os.path.join(vedir, 'bin', 'pip'), "install",
+     "--index-url=http://pypi.ccnmtl.columbia.edu/",
+     "wheel==0.21.0"])
 
+if ret:
+    exit(ret)
 
 ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                       "-E", vedir,
-                       "--index-url=''",
-                       "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+                       "--use-wheel",
+                       "--index-url=http://pypi.ccnmtl.columbia.edu/",
+                       "--requirement",os.path.join(pwd,"requirements.txt")])
 if ret: exit(ret)
-
-if sys.version_info < (2, 7, 0):
-    ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                           "-E", vedir,
-                           os.path.join(pwd,"requirements/src/importlib-1.0.1.tar.gz")])
-
 
 ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
 # --relocatable always complains about activate.csh, which we don't really
 # care about. but it means we need to ignore its error messages
+
+

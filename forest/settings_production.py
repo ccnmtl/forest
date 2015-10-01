948daafbfdf56a77dd5f141164b7a5802e9bf062
Anders Pearson <anders@columbia.edu>
2015-10-01 15:41:26 +0200

    switch to ccnmtlsettings

--- forest/settings_production.py
+++ forest/settings_production.py
@@ -1,37 +1,15 @@
 # flake8: noqa
 from settings_shared import *
+from ccnmtlsettings.production import common
 
-TEMPLATE_DIRS = (
-    "/var/www/forest/forest/forest/templates",
-)
-
-MEDIA_ROOT = '/var/www/forest/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/forest/forest/sitemedia'),
-)
-
-COMPRESS_ROOT = "/var/www/forest/forest/media/"
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'forest',
-        'HOST': '',
-        'PORT': 6432,
-        'USER': '',
-        'PASSWORD': '',
-        }
-}
-
-STATICFILES_DIRS = ()
-STATIC_ROOT = "/var/www/forest/forest/media/"
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+locals().update(
+    common(
+        project=project,
+        base=base,
+        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,
+        s3static=False,
+))
 
 SEED_STAND = "forest.ccnmtl.columbia.edu"
 
@@ -39,3 +17,5 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+
--- forest/settings_shared.py
+++ forest/settings_shared.py
@@ -1,27 +1,16 @@
 # Django settings for forest project.
 import os.path
 import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
-ADMINS = ()
+project = 'forest'
+base = os.path.dirname(__file__)
 
-MANAGERS = ADMINS
-
-ALLOWED_HOSTS = ['.ccnmtl.columbia.edu', 'localhost']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'forest',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
+locals().update(common(
+    project=project,
+    base=base,
+))
 
 CACHES = {
     'default': {
@@ -31,91 +20,22 @@ CACHES = {
 }
 
 if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
             }
         }
 
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
 PROJECT_APPS = [
     'forest.main',
 ]
 
 USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/forest/uploads/"
-MEDIA_URL = '/uploads/'
-STATIC_URL = '/media/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'stagingcontext.staging_processor',
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.template.context_processors.static',
-    'django.template.context_processors.media',
-    'django.contrib.messages.context_processors.messages',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-ROOT_URLCONF = 'forest.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/forest/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
 
 EPUB_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates/epub/")
 
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [  # noqa
     'sorl.thumbnail',
-    'django.contrib.admin',
     'tagging',
     'pagetree',
     'pageblocks',
@@ -123,16 +43,8 @@ INSTALLED_APPS = [
     'quizblock',
     'careermapblock',
     'fridgeblock',
-    'compressor',
-    'django_statsd',
     'bootstrapform',
-    'debug_toolbar',
-    'smoketest',
-    'django_jenkins',
-    'waffle',
-    'impersonate',
     'django_extensions',
-    'django_markwhat',
     'likertblock',
 ]
 
@@ -151,50 +63,9 @@ PAGEBLOCKS = ['pageblocks.TextBlock',
 EPUB_ALLOWED_BLOCKS = [
     'Text Block', 'HTML Block', 'Pull Quote']
 
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-)
-
-SENTRY_REMOTE_URL = 'http://sentry.ccnmtl.columbia.edu/sentry/store/'
-# remember to set the SENTRY_KEY in a local_settings.py
-# as documented in the wiki
-SENTRY_SITE = 'forest'
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[forest] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "forest@ccnmtl.columbia.edu"
-
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
-STATIC_ROOT = ""
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-COMPRESS_PARSER = "compressor.parser.HtmlParser"
-
 COMPRESS_PRECOMPILERS = (
     ('text/less', 'lessc {infile} {outfile}'),
 )
-
 
 # WIND settings
 
@@ -202,32 +73,5 @@ AUTHENTICATION_BACKENDS = ('djangowind.auth.WindAuthBackend',
                            'django.contrib.auth.backends.ModelBackend',)
 WIND_BASE = "https://wind.columbia.edu/"
 WIND_SERVICE = "cnmtl_full_np"
-WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
-                       'djangowind.auth.StaffMapper',
-                       'djangowind.auth.SuperuserMapper']
-WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
-WIND_SUPERUSER_MAPPER_GROUPS = ['anp8', 'jb2410', 'zm4', 'sbd12', 'egr2107',
-                                'kmh2124', 'sld2131', 'amm8', 'mar227',
-                                'ed2198', 'ej2223', 'njn2118']
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'forest'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
-
-LOGIN_REDIRECT_URL = "/"
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
 
 SEED_STAND = "test.example.com"
